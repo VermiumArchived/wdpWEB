4e71a99444765734e745ff8c12fa948cb3aa0c33
Restyled.io <commits@restyled.io>
2020-09-28 15:13:35 +0000

    Restyled by clang-format

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div>
-      <h1>
-        {"What's Dis Pic <br/>Web Application"}
-      </h1>
-    </div>
-  );
+  return (<div><h1>{"What's Dis Pic <br/>Web Application"}</h1>
+    </div>);
 }
 
 export default App;
