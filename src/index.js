546d15109ec2422dc4c9e2cc8f8f15e7f7470aa7
Restyled.io <commits@restyled.io>
2020-09-28 16:13:21 +0000

    Restyled by clang-format

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Router } from 'react-router-dom';
-import { render } from 'react-dom';
-
-import { history } from './_helpers';
-import { App } from './app/index';
-
 import './styles.less';
 
+import React from 'react';
+import {render} from 'react-dom';
+import {Router} from 'react-router-dom';
+
+import {history} from './_helpers';
+import {App} from './app/index';
+
 // setup fake backend
-//import { configureFakeBackend } from './_helpers';
-//configureFakeBackend();
+// import { configureFakeBackend } from './_helpers';
+// configureFakeBackend();
 
 render(
     <Router history={history}>
